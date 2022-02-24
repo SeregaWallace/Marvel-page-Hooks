a672435c50b7592158d090bb63377fe54ebabaf0
Serega Kordeluk <serega.wallace@gmail.com>
2022-02-24 15:57:13 +0300

    modified app js

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,36 +1,24 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
 
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null);
-
-    const onSelectChar = (id) => {
-        setChar(id);
-    }
 
     return (
+       <Router>
         <div className="app">
             <AppHeader/>
             <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <CharList onSelectChar={onSelectChar}/>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <Routes>
+                    <Route path="/comics" element={<ComicsPage/>}/>
+                    <Route path="/" element={<MainPage/>}/>
+                </Routes>
             </main>
         </div>
+       </Router>
     )
 
 }
