67f9e730991b6dbfa4b61f566ceb095fc518b0aa
Serega Kordeluk <serega.wallace@gmail.com>
2022-02-24 19:08:23 +0300

    page 404

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import MainPage from "../pages/MainPage";
 import ComicsPage from "../pages/ComicsPage";
+import Page404 from "../pages/404";
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
                 <Routes>
                     <Route path="/comics" element={<ComicsPage/>}/>
                     <Route path="/" element={<MainPage/>}/>
+                    <Route path="*" element={<Page404/>}/>
                 </Routes>
             </main>
         </div>
